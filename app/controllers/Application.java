f8dd1588ac0294e02b6bb1d300a56db5d6d812b7
Cedric Van Goethem <vangoethem.c@gmail.com>
2014-02-14 15:34:41 +0100

    Added bootstrap

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Zelensis"));
+        return ok(index.render());
     }
 
 }
