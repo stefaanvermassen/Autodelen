b5f31ca18447c71ff42dc3aca9e9e48152a3cd73
Laurent Bulcke <dikkevriend@hotmail.com>
2014-02-26 15:43:49 +0100

    Added UserRoles support

--- app/database/UserDAO.java
+++ app/database/UserDAO.java
@@ -1,7 +1,10 @@
 package database;
 
+import java.util.EnumSet;
+
 import models.Address;
 import models.User;
+import models.UserRole;
 
 /**
  * Created by Cedric on 2/16/14.
@@ -10,4 +13,5 @@ public interface UserDAO {
     public User getUser(String email) throws DataAccessException;
     public void updateUser(User user) throws DataAccessException;
     public User createUser(String email, String password, String firstName, String lastName, String phone, Address address) throws DataAccessException;
+    public User createUser(String email, String password, String firstName, String lastName, String phone, Address address, EnumSet<UserRole> roles) throws DataAccessException;
 }
--- app/database/jdbc/JDBCUserDAO.java
+++ app/database/jdbc/JDBCUserDAO.java
@@ -8,6 +8,11 @@ import models.User;
 import models.UserRole;
 
 import java.sql.*;
+import java.util.EnumSet;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.PreAction;
+
+import scalaz.std.effect.sql.preparedStatement;
 
 /**
  * Created by Cedric on 2/16/14.
@@ -19,11 +24,38 @@ public class JDBCUserDAO implements UserDAO {
     private Connection connection;
     private PreparedStatement getUserByEmailStatement;
     private PreparedStatement createUserStatement;
+    private PreparedStatement getUserRoles;
+    private PreparedStatement updateUserStatement;
+    private PreparedStatement insertUserRolesStatement;
+    private PreparedStatement deleteUserRolesStatement;
 
     public JDBCUserDAO(Connection connection) {
         this.connection = connection;
     }
+    
+    private PreparedStatement deleteUserRolesStatement() throws SQLException{
+    	if(deleteUserRolesStatement == null){
+    		deleteUserRolesStatement = connection.prepareStatement("DELETE FROM UserRoles WHERE userrole_userid=? AND userrole_role=?");    		
+    	}
+    	return deleteUserRolesStatement;
+    }
+    
+    private PreparedStatement insertUserRolesStatement() throws SQLException{
+    	if(insertUserRolesStatement == null){
+    		insertUserRolesStatement = connection.prepareStatement("IF NOT EXISTS (SELECT * FROM UserRoles WHERE userrole_userid"
+    				+ "=? AND userrole_role=?) INSERT INTO UserRoles(userrole_userid, userrole_role) VALUES (?,?) ");    		
+    	}
+    	return insertUserRolesStatement;
+    }
 
+    private PreparedStatement getUserRoles() throws SQLException {
+    	if(getUserRoles == null){
+    		getUserRoles = connection.prepareStatement("SELECT userrole_role FROM UserRoles INNER JOIN Users ON "
+    				+ "userrole_userid = user_id WHERE user_id = ?");
+    	}
+    	return getUserRoles;
+    }
+    
     private PreparedStatement getUserByEmailStatement() throws SQLException {
         if (getUserByEmailStatement == null) {
             getUserByEmailStatement = connection.prepareStatement("SELECT user_id, user_password, user_firstname, user_lastname, user_phone, user_email, " +
@@ -39,6 +71,13 @@ public class JDBCUserDAO implements UserDAO {
         }
         return createUserStatement;
     }
+    
+    private PreparedStatement updateUserStatement() throws SQLException {
+    	if (updateUserStatement == null){
+    		updateUserStatement = connection.prepareStatement("UPDATE Users SET user_email=?, user_password=?, user_firstname=?, user_lastname=?, user_phone=?, user_address_domicile_id=? ");
+    	}
+    	return updateUserStatement;
+    }
 
     public static User populateUser(ResultSet rs, boolean withPassword, boolean withAddress) throws SQLException {
         return new User(rs.getInt("user_id"), rs.getString("user_email"), rs.getString("user_firstname"), rs.getString("user_lastname"),
@@ -53,7 +92,18 @@ public class JDBCUserDAO implements UserDAO {
             ps.setString(1, email);
             try (ResultSet rs = ps.executeQuery()) {
                 rs.next();
-                return populateUser(rs, true, true);
+                User user = populateUser(rs, true, true);
+                PreparedStatement rolesStmt = getUserRoles();
+                rolesStmt.setInt(1, user.getId());
+                try (ResultSet roleSet = rolesStmt.executeQuery()) {
+                	while(roleSet.next()){
+                		user.addRole(UserRole.valueOf(rs.getString("userrole_role")));
+                	}
+                return user;	
+                } catch (SQLException ex){
+                	throw new DataAccessException("Error reading UserRoles", ex);
+                }
+                
             } catch (SQLException ex) {
                 throw new DataAccessException("Error reading user resultset", ex);
             }
@@ -87,9 +137,66 @@ public class JDBCUserDAO implements UserDAO {
             throw new DataAccessException("Failed to commit new user transaction.", ex);
         }
     }
+    
+    @Override
+    public User createUser(String email, String password, String firstName, String lastName, String phone, Address address, EnumSet<UserRole> roles) throws DataAccessException {
+        try (PreparedStatement ps = getCreateUserStatement() ; PreparedStatement insert = insertUserRolesStatement()) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ps.setString(3, firstName);
+            ps.setString(4, lastName);
+            ps.setString(5, phone);
+
+            if (address != null) {
+                ps.setInt(6, address.getId());
+            } else ps.setNull(6, Types.INTEGER);
+
+            ps.executeUpdate();
+            User user = new User(0, email, firstName, lastName, password, address);
+            for(UserRole role : roles){
+    			insert.setString(2, role.toString());
+    			insert.executeUpdate();
+    			user.addRole(role);
+        	}
+            try (ResultSet keys = ps.getGeneratedKeys()) {
+                keys.next(); //if this fails we want an exception anyway
+                user.setId(keys.getInt(1));
+                return user;
+            } catch (SQLException ex) {
+                throw new DataAccessException("Failed to get primary key for new user.", ex);
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to commit new user transaction.", ex);
+        }
+    }
 
     @Override
     public void updateUser(User user) throws DataAccessException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    	try (PreparedStatement ps = getCreateUserStatement() ; PreparedStatement insert = insertUserRolesStatement() ; PreparedStatement delete = deleteUserRolesStatement()) {
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getFirstName());
+            ps.setString(4, user.getLastName());
+            ps.setString(5, user.getPhone());
+
+            if (user.getAddress() != null) {
+                ps.setInt(6, user.getAddress().getId());
+            } else ps.setNull(6, Types.INTEGER);
+
+            ps.executeUpdate();
+            
+            insert.setInt(1, user.getId());
+        	delete.setInt(1, user.getId());
+        	for(UserRole role : UserRole.values()){
+        		if(user.gotRole(role)){
+        			insert.setString(2, role.toString());
+        			insert.executeUpdate();
+        		} else {
+        			delete.setString(2, role.toString());
+        		}
+        	}
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to update user", ex);
+        }
     }
 }
