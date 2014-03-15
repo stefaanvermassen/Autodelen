655d51cdbd0adc414f5b2d3f2d65e08db6899180
Stefaan Vermassen <Stefaan.Vermassen@UGent.be>
2014-03-15 19:32:09 +0100

    New notifications table

--- sql/implementation.sql
+++ sql/implementation.sql
@@ -221,7 +221,7 @@ CREATE TABLE `CarRides` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB;
 
-CREATE TABLE `Messages` (
+CREATE TABLE `Messages` ( # from user to user != Notifications
 	`message_id` INT NOT NULL AUTO_INCREMENT,
 	`message_from_user_id` INT NOT NULL,
 	`message_to_user_id` INT NOT NULL,
@@ -272,3 +272,15 @@ CREATE TABLE `Verifications` (
 	PRIMARY KEY (`verification_user_id`, `verification_type`),
 	CONSTRAINT `FK_VERIFICATION_USER` FOREIGN KEY (`verification_user_id`) REFERENCES `Users` (`user_id`)
 )
+
+CREATE TABLE `Notifications`{
+	`notification_id` INT NOT NULL AUTO_INCREMENT,
+	`notification_user_id` INT NOT NULL,
+	`notification_subject` VARCHAR(255),
+	`notification_read` BIT(1) NOT NULL DEFAULT '0',
+	`notification_body` TEXT,
+	`notification_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`notification_id`),
+	FOREIGN KEY (`notification_user_id`) REFERENCES Users(`user_id`)
+}
+
