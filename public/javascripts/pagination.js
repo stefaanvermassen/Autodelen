15493e9b7a0c58b33a7974a3c962edcc045fad09
hmareen <hannes.mareen@ugent.be>
2014-03-18 23:22:48 +0100

    Polished version of pagination and sorting

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,24 +1,27 @@
+/* Variables we can overwrite after we included the script */
 var previousBtnTxt = "Vorige";
 var nextBtnTxt = "Volgende";
 var firstBtnTxt = "<<";
 var lastBtnTxt = ">>";
 
+// For example: 2 means if we are at page 5, we will see: 3 4 5 6 7. If we are at page 1 we will see: 1 2 3 4 5
 var buttonsAroundPage = 2;
 
-var page = 1;
-var asc = 1; // true
-var orderBy = 0;
-$(document).ready(loadPage());
+$(document).ready(loadPage(1, 1, 0));
 
-function loadPage() {
+function loadPage(page, asc, orderBy) {
     route(page, asc, orderBy).ajax({
         success : function(html) {
             $("#carsTable").html(html);
 
-            /* Navigation buttons */
+            /*
+             * Navigation buttons
+             *
+             * These will come in the element with id="buttons"
+             */
             // Button to go to first page and to previous page
-            var buttonString = "<button id='firstPage' type='button'>" + firstBtnTxt + "</button> " +
-                "<button id='previousPage' type='button'>" + previousBtnTxt + "</button> ";
+            var buttonString = "<button class='buttons' id='firstPage' name='1' type='button'>" + firstBtnTxt + "</button> " +
+                "<button class='buttons' id='previousPage' name='" + (page - 1)  + "' type='button'>" + previousBtnTxt + "</button> ";
 
             // Calculate how many previous pages we create buttons to (standard 2, but less if we can't go back more, or more when we can't go further more -> max 4)
             var previousPages = buttonsAroundPage;
@@ -28,108 +31,72 @@ function loadPage() {
             }
             while(previousPages >= 1) {
                 if(page - previousPages >= 1) {
-                    buttonString += "<button id='previousPage" +  previousPages + "' name='" + previousPages + "' type='button'>" + (page - previousPages) + "</button> ";
+                    buttonString += "<button class='buttons' id='previousPage" +  previousPages + "' name='" + (page - previousPages) + "' type='button'>" + (page - previousPages) + "</button> ";
                     amountOfPreviousPages++;
                 }
                 previousPages--;
             }
             // Button for current page. Disabled ofcourse.
-            buttonString += "<button id='currentPage' type='button' disabled='disabled'>" + page + "</button> ";
+            buttonString += "<button class='buttons' id='currentPage' name='" + page + "' type='button'>" + page + "</button> ";
 
             // Calculate how many next pages we create buttons to (standard 2, but less if we can't go further more, or more when we can't go back more -> max 4)
             var nextPages = 1;
             while(page + nextPages <= amountOfPages && nextPages <= buttonsAroundPage + (buttonsAroundPage - amountOfPreviousPages)) {
-                buttonString += "<button id='nextPage" +  nextPages + "' name='" + nextPages + "' type='button'>" + (page + nextPages) + "</button> ";
+                buttonString += "<button class='buttons' id='nextPage" +  nextPages + "' name='" + (page + nextPages) + "' type='button'>" + (page + nextPages) + "</button> ";
                 nextPages++;
             }
 
             // Button to go to last page and next page
-            buttonString += "<button id='nextPage' type='button'>" + nextBtnTxt + "</button> " +
-                "<button id='lastPage' type='button'>" + lastBtnTxt + "</button>"
+            buttonString += "<button class='buttons' id='nextPage' name='" + (page + 1)  + "' type='button'>" + nextBtnTxt + "</button> " +
+                "<button class='buttons' id='lastPage' name='" + amountOfPages + "' type='button'>" + lastBtnTxt + "</button>";
 
             // Add the buttons to the html-file
             $("#buttons").html(buttonString);
 
-
-            // Now let's add the appropriote onclick-functions to the buttons
-            var firstButton = document.getElementById("firstPage");
-            var previousButton = document.getElementById("previousPage");
-            if(page == 1) {
-                firstButton.setAttribute("disabled", "disabled");
-                previousButton.setAttribute("disabled", "disabled");
-            } else {
-                firstButton.onclick = function(){
-                    page = 1;
-                    loadPage();
-                };
-                previousButton.onclick = function(){
-                    page--;
-                    loadPage();
-                };
-            }
-
-            var previousPages = buttonsAroundPage;
-            var amountOfPreviousPages = 0;
-            if(amountOfPages - page < buttonsAroundPage) {
-                previousPages += buttonsAroundPage - (amountOfPages - page);
-            }
-            while(previousPages >= 1) {
-                if(page - previousPages >= 1) {
-                    var previousButton = document.getElementById("previousPage" + previousPages);
-                    previousButton.onclick = function() {
-                        var minus = parseInt(this.getAttribute("name"));
-                        page = page - minus;
-                        loadPage();
-                    };
-                    amountOfPreviousPages++;
+            // Now let's add the appropriote onclick-functions to the buttons and disable them if needed
+            var buttons = document.getElementsByClassName('buttons');
+            for(var i = 0; i < buttons.length; i++) {
+                var p = parseInt(buttons[i].getAttribute("name"));
+                if(p < 1 || p > amountOfPages || p == page) {
+                    buttons[i].setAttribute("disabled", "disabled");
+                } else {
+                    buttons[i].onclick = function() {
+                        var p = parseInt(this.getAttribute("name"));
+                        loadPage(p, asc, orderBy);
+                    }
                 }
-                previousPages--;
             }
 
-            var nextPages = 1;
-            while(page + nextPages <= amountOfPages && nextPages <= buttonsAroundPage + (buttonsAroundPage - amountOfPreviousPages)) {
-                var nextButton = document.getElementById("nextPage" + nextPages);
-                nextButton.onclick = function() {
-                    var plus = parseInt(this.getAttribute("name"));
-                    page = page + plus;
-                    loadPage();
-                };
-                nextPages++;
-            }
-
-            var nextButton = document.getElementById("nextPage");
-            var lastButton = document.getElementById("lastPage");
-            if(page == amountOfPages) {
-                nextButton.setAttribute("disabled", "disabled");
-                lastButton.setAttribute("disabled", "disabled");
-            } else {
-                nextButton.onclick = function(){
-                    page++;
-                    loadPage();
-                };
-                lastButton.onclick = function(){
-                    page = amountOfPages;
-                    loadPage();
-                };
-            }
-
-            /* Sorting */
-            var orderBys = new Array();
+            /*
+             * Sorting
+             *
+             * All th-elements that we want to sort on have to have an id = "sortableX"
+             * Where X is a number between 1 and amountOfSortables
+             * It also has to have a name attribute that will be used in the controller to know what to sort on
+             */
             for (var i=1; i <= amountOfSortables; i++) {
                 var sortable = document.getElementById("sortable" + i);
                 sortable.onclick = function() {
-                    orderBy = this.getAttribute("name");
-                    asc = (asc + 1) % 2;
+                    var orderByNew = this.getAttribute("name");
+                    if(orderBy == orderByNew) {
+                        // Change order asc <-> desc if we click on the column that already is ordered
+                        asc = (asc + 1) % 2;
+                    } else { // Else do asc order
+                        asc = 1;
+                    }
                     page = 1;
-                    loadPage();
+                    loadPage(page, asc, orderByNew);
                 };
+
+                // Set class of sorted column to asc or desc (so we can style with css)
+                if(sortable.getAttribute("name") == orderBy) {
+                    sortable.setAttribute("class", sortable.getAttribute("class" + " " + (asc == 1 ? "asc" : "desc")));
+                }
             }
 
-            // By default we orderBy the first sortable column
-            if(orderBy == 0) {
-                orderBy = $('#sortable1').attr('name');
-            }
-
+            /*
+             * TODO: Filtering
+             */
         }
     });
 }
