6dc83ad470c942b7af35c762cb9ecb0662983069
Stefaan Vermassen <Stefaan.Vermassen@UGent.be>
2014-03-21 00:31:13 +0100

    Shows now notifications and unread number in dropdown menu

--- app/database/providers/NotificationProvider.java
+++ app/database/providers/NotificationProvider.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class NotificationProvider {
 
     private static final String NOTIFICATIONS_BY_ID = "notification:id:%d";
+    private static final String NOTIFICATION_NUMBER_BY_ID = "notification_number:id:%d";
     private DataAccessProvider provider;
     private UserProvider userProvider;
 
@@ -55,5 +56,41 @@ public class NotificationProvider {
         } else {
             return (List<Notification>) obj; //Type erasure problem from Java, works at runtime
         }
+    }
+
+    public int getNumberOfUnreadNotifications(int userId){
+        return getNumberOfUnreadNotifications(userId, true);
+    }
+    public int getNumberOfUnreadNotifications(int userId, boolean cached){
+        String key = String.format(NOTIFICATION_NUMBER_BY_ID, userId);
+        Object obj = null;
+        if (cached) {
+            obj = Cache.get(key);
+        }
+        if (obj == null || !(obj instanceof List)) {
+            try (DataAccessContext context = provider.getDataAccessContext()) {
+                NotificationDAO dao = context.getNotificationDAO();
+                int unread_number = -1;
+                unread_number = dao.getNumberOfUnreadNotifications(userId);
+                if (unread_number != -1) {
+                    Cache.set(key, unread_number);
+                    return unread_number;
+                } else {
+                    return -1;
+                }
+            } catch (DataAccessException ex) {
+                throw ex;
+            }
+        } else {
+            return (Integer) obj; //Type erasure problem from Java, works at runtime
+        }
+    }
+
+    public void invalidateNotifications(int userId){
+        Cache.remove(String.format(NOTIFICATIONS_BY_ID, userId));
+    }
+
+    public void invalidateNotificationNumber(int userId){
+        Cache.remove(String.format(NOTIFICATION_NUMBER_BY_ID, userId));
     }
 }
