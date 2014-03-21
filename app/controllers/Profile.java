03704f60b88864ad6e4bdf747d14eab3fde04427
Cedric Van Goethem <vangoethem.c@gmail.com>
2014-03-21 21:54:50 +0100

    Start over with profile editor

--- app/controllers/Profile.java
+++ app/controllers/Profile.java
@@ -5,6 +5,7 @@ import database.DataAccessContext;
 import database.DataAccessException;
 import database.DatabaseHelper;
 import database.UserDAO;
+import models.Address;
 import models.User;
 import models.UserRole;
 import play.data.Form;
@@ -21,9 +22,58 @@ public class Profile extends Controller {
         }
     }
 
+    public static class EditAddressModel { //TODO: unify with other models in controllers
+
+        public String city;
+
+        public void populate(Address address){
+            if(address == null)
+                return;
+
+            city = address.getCity();
+        }
+    }
+
+    public static class EditProfileModel {
+        public String phone;
+        public String firstName;
+        public String lastName;
+        public String email; // TODO: verification
+
+        public String identityCardNumber;
+        public String nationalNumber;
+
+        public EditAddressModel domicileAddress;
+        public EditAddressModel residenceAddress;
+
+        public EditProfileModel(){
+            this.domicileAddress = new EditAddressModel();
+            this.residenceAddress = new EditAddressModel();
+        }
+
+        public void populate(User user){
+            if(user == null)
+                return;
+
+            this.email = user.getEmail();
+            this.firstName = user.getFirstName();
+            this.lastName = user.getLastName();
+            this.phone = user.getPhone();
+
+            this.domicileAddress.populate(user.getAddressDomicile());
+            this.residenceAddress.populate(user.getAddressResidence());
+        }
+    }
+
     @RoleSecured.RoleAuthenticated()
     public static Result indexWithoutId(){
-        return redirect(routes.Profile.index(DatabaseHelper.getUserProvider().getUser(session("email")).getId()));
+        try(DataAccessContext context = DatabaseHelper.getDataAccessProvider().getDataAccessContext()) {
+            UserDAO dao = context.getUserDAO();
+            User user = dao.getUser(session("email")); //user always has to exist (roleauthenticated)
+            return ok(index.render(user));
+        } catch(DataAccessException ex){
+            throw ex;
+        }
     }
 
     @RoleSecured.RoleAuthenticated()
@@ -31,58 +81,39 @@ public class Profile extends Controller {
         try (DataAccessContext context = DatabaseHelper.getDataAccessProvider().getDataAccessContext()) {
             UserDAO dao = context.getUserDAO();
             User user = dao.getUser(userId, true);
+
+            if(user == null){
+                flash("danger", "GebruikersID " + userId + " bestaat niet.");
+                return redirect(routes.Dashboard.index());
+            }
+
             User currentUser = DatabaseHelper.getUserProvider().getUser(session("email"));
 
             // Only a profile admin or
             if (currentUser.getId() != user.getId() && !DatabaseHelper.getUserRoleProvider().hasRole(user.getId(), UserRole.PROFILE_ADMIN)) {
-                flash("danger", "U heeft geen rechten tot deze profielpagina.");
-                return redirect(routes.Application.index());
+                return badRequest(views.html.unauthorized.render(new UserRole[] {UserRole.PROFILE_ADMIN }));
             }
 
-            // Prepare small form on the left
-            EditSmallProfileModel smallModel = new EditSmallProfileModel();
-            smallModel.phone = user.getPhone();
-            Form<EditSmallProfileModel> smallForm = Form.form(EditSmallProfileModel.class).fill(smallModel);
-
-            return ok(index.render(user, smallForm));
+            return ok(index.render(user));
         } catch (DataAccessException ex) {
             throw ex;
         }
     }
 
-    /**
-     * Method: POST
-     *
-     * @param userId
-     * @return
-     */
     @RoleSecured.RoleAuthenticated()
-    public static Result editSmallProfilePost(int userId) {
+    public static Result edit(int userId){
         try (DataAccessContext context = DatabaseHelper.getDataAccessProvider().getDataAccessContext()) {
             UserDAO dao = context.getUserDAO();
             User user = dao.getUser(userId, true);
             User currentUser = DatabaseHelper.getUserProvider().getUser(session("email"));
 
             if (currentUser.getId() != user.getId() && !DatabaseHelper.getUserRoleProvider().hasRole(user.getId(), UserRole.PROFILE_ADMIN)) {
-                flash("danger", "U heeft geen rechten tot deze profielpagina.");
-                return redirect(routes.Application.index());
+                return badRequest(views.html.unauthorized.render(new UserRole[] {UserRole.PROFILE_ADMIN }));
             }
 
-            Form<EditSmallProfileModel> smallForm = Form.form(EditSmallProfileModel.class).bindFromRequest();
-            if (smallForm.hasErrors()) {
-                return badRequest(index.render(user, smallForm));
-            } else {
-                try {
-                    user.setPhone(smallForm.get().phone);
-                    dao.updateUser(user, true); //TODO: this shouldn't be a 'withrest'!!
-                    context.commit();
-                    flash("success", "Het profiel werd succesvol aangepast.");
-                    return redirect(routes.Profile.index(user.getId()));
-                } catch (DataAccessException ex) {
-                    context.rollback();
-                    throw ex;
-                }
-            }
+            EditProfileModel model = new EditProfileModel();
+            model.populate(user);
+            return ok(edit.render(Form.form(EditProfileModel.class).fill(model), userId));
         } catch (DataAccessException ex) {
             throw ex;
         }
