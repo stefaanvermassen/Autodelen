24faf4a8d4a1473a1f29b71c3dca46f702923c8f
Stefaan Vermassen <Stefaan.Vermassen@UGent.be>
2014-04-26 23:12:05 +0200

    First version of scheduler

---
+++ app/schedulers/Scheduler.java
@@ -0,0 +1,23 @@
+package schedulers;
+
+import play.libs.Akka;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Stefaan Vermassen on 26/04/14.
+ */
+public abstract class Scheduler implements Runnable{
+
+    public void schedule(FiniteDuration repeatDuration){
+        Akka.system().scheduler().schedule(
+                Duration.create(0, TimeUnit.MILLISECONDS), //Initial delay 0 milliseconds
+                repeatDuration,     //Frequency 30 minutes
+                this,
+                Akka.system().dispatcher()
+        );
+
+    }
+}
---
+++ app/schedulers/SendUnreadNotificationsMailScheduler.java
@@ -0,0 +1,15 @@
+package schedulers;
+
+/**
+ * Created by Stefaan Vermassen on 26/04/14.
+ */
+public class SendUnreadNotificationsMailScheduler extends Scheduler{
+
+    /*Send mail to user if he has unread notifications for a certain period of time,
+    * or if he has more than 3
+    * */
+    @Override
+    public void run() {
+
+    }
+}
