4a530da58da0801a40fc025fbae3c7131791c5e3
Stefaan Vermassen <Stefaan.Vermassen@UGent.be>
2014-04-27 13:21:19 +0200

    Updated query

--- app/database/jdbc/JDBCSchedulerDAO.java
+++ app/database/jdbc/JDBCSchedulerDAO.java
@@ -25,17 +25,18 @@ public class JDBCSchedulerDAO implements SchedulerDAO {
 
     private PreparedStatement getGetReminderEmailListStatement() throws SQLException {
         if (getReminderEmailListStatement == null) {
-            getReminderEmailListStatement = connection.prepareStatement("SELECT notification_email AS user_email " +
-                    "FROM (SELECT a.notification_user, a.notification_email, a.notification_last_notified, " +
+            getReminderEmailListStatement = connection.prepareStatement("SELECT * FROM " +
+                    "(SELECT a.user_id, a.user_email, a.user_firstname, a.user_lastname, a.user_last_notified, " +
                     "a.number_of_notifications, COUNT(b.message_id) AS number_of_messages " +
-                    "FROM (SELECT p.user_id AS notification_user, p.user_email " +
-                    "AS notification_email, p.user_last_notified AS notification_last_notified, " +
-                    "COUNT(o.notification_id) AS number_of_notifications " +
-                    "FROM Users p LEFT JOIN Notifications o ON o.notification_user_id = p.user_id GROUP BY p.user_id) a " +
-                    "LEFT JOIN MESSAGES b ON a.notification_user = b.message_from_user_id " +
-                    "GROUP BY a.notification_user) AS notfs " +
-                    "WHERE  (number_of_notifications > ? OR number_of_messages > ?) " +
-                    "AND notification_last_notified < DATE_SUB(NOW(),INTERVAL 7 DAY)");
+                    "FROM (SELECT p.user_id AS user_id, p.user_email AS user_email, p.user_firstname " +
+                    "AS user_firstname, p.user_lastname AS user_lastname, p.user_last_notified AS user_last_notified, " +
+                    "COUNT(o.notification_id) AS number_of_notifications FROM Users p " +
+                    "LEFT JOIN (SELECT * FROM Notifications WHERE notification_read=0) o " +
+                    "ON o.notification_user_id = p.user_id GROUP BY p.user_id) a " +
+                    "LEFT JOIN (SELECT * FROM Messages WHERE message_read=0) b " +
+                    "ON a.user_id = b.message_to_user_id GROUP BY a.user_id) AS Reminder " +
+                    "WHERE (number_of_notifications > 0 OR number_of_messages > 0) " +
+                    "AND user_last_notified < DATE_SUB(NOW(),INTERVAL 7 DAY)");
         }
         return getReminderEmailListStatement;
     }
