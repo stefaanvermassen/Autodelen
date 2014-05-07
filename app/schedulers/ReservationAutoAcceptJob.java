5811bd3375cdf1e2f36f3cfcc4ae5f10578da84c
Cedric Van Goethem <vangoethem.c@gmail.com>
2014-05-07 16:57:05 +0200

    New automatic reservation scheduling

--- app/schedulers/ReservationAutoAcceptJob.java
+++ app/schedulers/ReservationAutoAcceptJob.java
@@ -1,10 +1,12 @@
 package schedulers;
 
-import controllers.Drives;
+import database.DataAccessContext;
+import database.ReservationDAO;
 import models.Job;
 import models.Reservation;
 import models.ReservationStatus;
 import notifiers.Notifier;
+import providers.DataProvider;
 
 /**
  * Created by Cedric on 5/7/2014.
@@ -12,7 +14,24 @@ import notifiers.Notifier;
 public class ReservationAutoAcceptJob implements ScheduledJobExecutor {
     @Override
     public void execute(Job job) {
-        Reservation reservation = Drives.adjustStatus(job.getRefId(), ReservationStatus.ACCEPTED);
-        Notifier.sendReservationApprovedByOwnerMail(reservation.getUser(), "", reservation);
+        try (DataAccessContext context = DataProvider.getDataAccessProvider().getDataAccessContext()) {
+            ReservationDAO dao = context.getReservationDAO();
+            Reservation reservation = dao.getReservation(job.getRefId());
+            if(reservation == null) {
+                return;
+            }
+
+            if(reservation.getStatus() == ReservationStatus.REQUEST ) {
+                if(reservation.getFrom().isBeforeNow()) {
+                    reservation.setStatus(ReservationStatus.ACCEPTED);
+                    Notifier.sendReservationApprovedByOwnerMail(reservation.getUser(), "Automatisch goedgekeurd door systeem.", reservation);
+                } else {
+                    reservation.setStatus(ReservationStatus.CANCELLED);
+                }
+                dao.updateReservation(reservation);
+                context.commit();
+            }
+        }
+
     }
 }
