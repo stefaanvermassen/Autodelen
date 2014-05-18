de7cd9eceb2902b3c83fd47695fb8f3e8f37cc62
Stefaan Vermassen <Stefaan.Vermassen@UGent.be>
2014-05-19 00:34:39 +0200

    Initial data

--- sql/userroles.sql
+++ sql/userroles.sql
@@ -1,2 +1,2 @@
 INSERT INTO `userroles` (`userrole_userid`, `userrole_role`) VALUES 
-((SELECT user_id FROM users WHERE user_email='stefaanvermassen@zelensis.be'), 'SUPER_USER');
+((SELECT user_id FROM users WHERE user_email='autodelen.zelensis@gmail.com'), 'SUPER_USER');
