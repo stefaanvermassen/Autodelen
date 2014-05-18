51dd10cc81b36ba918c325cf1346571a7bc5ef19
Stefaan Vermassen <Stefaan.Vermassen@UGent.be>
2014-05-19 01:10:43 +0200

    Receipts in scheduler

--- app/schedulers/ReportGenerationJob.java
+++ app/schedulers/ReportGenerationJob.java
@@ -1,7 +1,17 @@
 package schedulers;
 
+import controllers.Receipts;
+import database.DataAccessContext;
+import database.DataAccessException;
+import database.FilterField;
+import database.UserDAO;
 import models.Job;
+import models.User;
 import play.Logger;
+import providers.DataProvider;
+
+import java.sql.Date;
+import java.util.List;
 
 /**
  * Created by Cedric on 5/3/2014.
@@ -9,6 +19,21 @@ import play.Logger;
 public class ReportGenerationJob implements ScheduledJobExecutor {
     @Override
     public void execute(Job job) {
-        Logger.debug("Generate report...");
+
+        try (DataAccessContext context = DataProvider.getDataAccessProvider().getDataAccessContext()) {
+            context.getCarRideDAO().endPeriod();
+            context.getRefuelDAO().endPeriod();
+            context.getCarCostDAO().endPeriod();
+
+            UserDAO dao = context.getUserDAO();
+
+            List<User> users = dao.getUserList(FilterField.USER_NAME, true, 1, dao.getAmountOfUsers(null), null);
+            context.commit();
+            for(User user : users) {
+                Receipts.generateReceipt(user, new Date(java.util.Calendar.getInstance().getTime().getTime()));
+            }
+        }catch(DataAccessException ex) {
+            throw ex;
+        }
     }
 }
